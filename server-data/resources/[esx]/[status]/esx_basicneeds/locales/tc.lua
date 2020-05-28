d2ca43e6f9af4668cd8b6098d39b155ec4b85862
YiShan8787 <59713684gx@gmail.com>
2020-05-28 22:46:57 +0800

    basic need

---
+++ server-data/resources/[esx]/[status]/esx_basicneeds/locales/tc.lua
@@ -0,0 +1,7 @@
+Locales['tc'] = {
+	['used_bread'] = '你吃了~y~一塊~s~~b~麵包~s~',
+	['used_water'] = '你喝了~y~一瓶~s~~b~礦泉水~s~',
+	['used_taco'] = '你吃了~y~一塊~s~~b~捲餅~s~',
+	['used_milktea'] = '你喝了~y~一杯~s~~b~奶茶~s~',
+	['used_frenchfries'] = '你吃了~y~一包~s~~b~薯條~s~',
+}
---
+++ server-data/resources/[esx]/[status]/esx_basicneeds/server/main.lua
@@ -0,0 +1,118 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterUsableItem('bread', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('bread', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 100000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_bread'))
+end)
+
+ESX.RegisterUsableItem('taco', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('taco', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 200000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_taco'))
+end)
+
+ESX.RegisterUsableItem('frenchfries', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('frenchfries', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 200000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_frenchfries'))
+end)
+
+ESX.RegisterUsableItem('milktea', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('milktea', 1)
+	xPlayer.addInventoryItem('bottle', 1)
+	TriggerClientEvent('esx_status:add', source, 'thirst', 200000)
+	TriggerClientEvent('esx_basicneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_milktea'))
+end)
+
+ESX.RegisterUsableItem('water', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('water', 1)
+	xPlayer.addInventoryItem('bottle', 1)
+	TriggerClientEvent('esx_status:add', source, 'thirst', 100000)
+	TriggerClientEvent('esx_basicneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_water'))
+end)
+
+
+
+ESX.RegisterUsableItem('BULLETPROOF', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	TriggerClientEvent('esx_weaponshop:BULLETPROOF', source)
+	xPlayer.removeInventoryItem('BULLETPROOF', 1)
+end)
+--[[
+TriggerEvent('es:addGroupCommand', 'heal', 'admin', function(source, args, user)
+	-- heal another player - don't heal source
+	if args[1] then
+		local playerId = tonumber(args[1])
+
+		-- is the argument a number?
+		if playerId then
+			-- is the number a valid player?
+			if GetPlayerName(playerId) then
+				print(('esx_basicneeds: %s healed %s'):format(GetPlayerIdentifier(source, 0), GetPlayerIdentifier(playerId, 0)))
+				TriggerClientEvent('esx_basicneeds:healPlayer', playerId)
+				TriggerClientEvent('chat:addMessage', source, { args = { '^5HEAL', 'You have been healed.' } })
+			else
+				TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Player not online.' } })
+			end
+		else
+			TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Invalid player id.' } })
+		end
+	else
+		print(('esx_basicneeds: %s healed self'):format(GetPlayerIdentifier(source, 0)))
+		TriggerClientEvent('esx_basicneeds:healPlayer', source)
+	end
+end, function(source, args, user)
+	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
+end, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', params = {{name = 'playerId', help = '(optional) player id'}}})
+--]]
+--[[
+ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
+	--if args[1] then
+		--local playerId = tonumber(args[1])
+		
+		if ESX.GetPlayerFromId(args.playerId) then
+			if GetPlayerName(args.playerId) then
+				--print(('esx_basicneeds: %s healed %s'):format(GetPlayerIdentifier(source, 0), GetPlayerIdentifier(playerId, 0)))
+				TriggerClientEvent('esx_basicneeds:healPlayer', args.playerId)
+				xPlayer.triggerEvent('chat:addMessage', source, { args = { '^5HEAL', 'You have been healed.' } })
+			else
+				xPlayer.triggerEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Player not online.' } })
+			end
+		else
+			--TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Player not online.' } })
+			xPlayer.triggerEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Player not online.' } })
+		end
+	--end
+
+end, true, {help = "幫復", validate = true, arguments = {
+    {name = 'playerId', help = "玩家ID", type = 'player'}
+
+}})
+
+ESX.RegisterCommand('healme', 'admin', function(xPlayer, args, showError)
+	xPlayer.triggerEvent('esx_basicneeds:healPlayer')
+end, true, {help = "回復體力", validate = true
+
+})
+--]]
