0a1cf584c558ec2936b93f024f3a787401b23df8
YiShan8787 <59713684gx@gmail.com>
2020-05-28 09:56:41 +0800

    billing

---
+++ server-data/resources/[esx]/esx_billing/client/main.lua
@@ -0,0 +1,127 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+ESX          = nil
+local isDead = false
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+function ShowBillsMenu()
+
+	ESX.TriggerServerCallback('esx_billing:getBills', function(bills)
+		ESX.UI.Menu.CloseAll()
+		local elements = {}
+
+		for i=1, #bills, 1 do
+			table.insert(elements, {
+				label  = ('%s - <span style="color:red;">%s</span>'):format(bills[i].label, _U('invoices_item', ESX.Math.GroupDigits(bills[i].amount))),
+				billID = bills[i].id
+			})
+		end
+
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'billing',
+		{
+			title    = _U('invoices'),
+			align    = 'bottom-right',
+			elements = elements
+		}, function(data, menu)
+			menu.close()
+
+			ESX.TriggerServerCallback('esx_billing:payBill', function()
+				ShowBillsMenu()
+			end, data.current.billID)
+		end, function(data, menu)
+			menu.close()
+		end)
+	end)
+
+end
+
+-- Key controls
+--[[
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsControlJustReleased(0, Keys['F7']) and not isDead and not ESX.UI.Menu.IsOpen('default', GetCurrentResourceName(), 'billing') then
+			ShowBillsMenu()
+		end
+	end
+end)
+]]--
+AddEventHandler('esx:onPlayerDeath', function(data)
+	isDead = true
+end)
+
+AddEventHandler('playerSpawned', function(spawn)
+	isDead = false
+end)
+
+function CreateBlip(coords)
+	local blip = AddBlipForCoord(coords)
+
+	SetBlipSprite(blip, 500)
+	SetBlipScale(blip, 0.9)
+	SetBlipColour(blip, 4)
+	--SetBlipDisplay(blip, 4)
+	SetBlipAsShortRange(blip, true)
+
+	BeginTextCommandSetBlipName("STRING")
+	--AddTextComponentString("Gas Station")
+	AddTextComponentString("國稅局")
+	EndTextCommandSetBlipName(blip)
+
+	return blip
+end
+
+Citizen.CreateThread(function()
+	for _, ZoneCoords in pairs(Config.Blip) do
+		CreateBlip(ZoneCoords)
+	end
+
+	while true do
+        local sleepThread = 500
+
+        local ped = PlayerPedId()
+        local pedCoords = GetEntityCoords(ped)
+
+        for locationIndex = 1, #Config.Zones do
+            local locationPos = Config.Zones[locationIndex]
+
+            local dstCheck = GetDistanceBetweenCoords(pedCoords, locationPos, true)
+
+            if dstCheck <= 5.0 then
+                sleepThread = 5
+
+                local text = "單據繳款處"
+
+                if dstCheck <= 3.0 then
+                    text = "[~g~E~s~] " .. text
+
+                    if IsControlJustReleased(0, 38) then
+						--TriggerServerEvent("esx-ecobottles:sellBottles")
+						ShowBillsMenu()
+                    end
+                end
+                
+                ESX.Game.Utils.DrawText3D(locationPos, text, 1)
+            end
+        end
+
+        Citizen.Wait(sleepThread)
+    end
+
+end)
