4cd25194fb32f9d42e60ba87fa7d840c9446aef1
YiShan8787 <59713684gx@gmail.com>
2020-05-28 10:38:40 +0800

    prison

---
+++ server-data/resources/[map]/[prison]/int_prisonfull/server.lua
@@ -0,0 +1,15 @@
+local is_on = false
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('int_prisonfull:setprison')
+AddEventHandler('int_prisonfull:setprison', function(flag)
+    is_on = flag
+end)
+
+ESX.RegisterServerCallback('int_prisonfull:getflag', function(source, cb)
+	--local xPlayer = ESX.GetPlayerFromId(target)
+
+	cb(is_on)
+end)
