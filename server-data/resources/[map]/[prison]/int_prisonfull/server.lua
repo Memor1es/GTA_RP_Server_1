b5aede4984febb2cb7f4cc0eb6d545894b3b480e
YiShan8787 <59713684gx@gmail.com>
2020-05-28 12:45:25 +0800

    alarm

--- server-data/resources/[map]/[prison]/int_prisonfull/server.lua
+++ server-data/resources/[map]/[prison]/int_prisonfull/server.lua
@@ -6,19 +6,20 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterNetEvent('int_prisonfull:setprison')
 AddEventHandler('int_prisonfull:setprison', function(flag)
     is_on = flag
-    
-    for _, player in ipairs(GetActivePlayers()) do
-        local ped = GetPlayerPed(player)
+    local ptable = GetActivePlayers()
+    if ptable then
+        for _, player in ipairs(ptable) do
+            local ped = GetPlayerPed(player)
 
-        if DoesEntityExist(ped) then
-            if is_on then
-                TriggerClientEvent('int_prisonfull:on',player)
-            else
-                TriggerClientEvent('int_prisonfull:off',player)
+            if DoesEntityExist(ped) then
+                if is_on then
+                    TriggerClientEvent('int_prisonfull:on',player)
+                else
+                    TriggerClientEvent('int_prisonfull:off',player)
+                end
             end
         end
     end
-    
 end)
 
 ESX.RegisterServerCallback('int_prisonfull:getflag', function(source, cb)
