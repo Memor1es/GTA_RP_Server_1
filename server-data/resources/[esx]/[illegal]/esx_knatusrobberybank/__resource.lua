5d8fa9122c436928e5b6be12c14fa1c37cb4ccdd
YiShan8787 <59713684gx@gmail.com>
2020-06-04 20:52:40 +0800

    bank

---
+++ server-data/resources/[esx]/[illegal]/esx_knatusrobberybank/__resource.lua
@@ -0,0 +1,27 @@
+--dependency 'essentialmode'
+dependency 'es_extended'
+dependency 'esx_doorlock_mhacking'
+dependency 'esx_knatusblowtorch'
+dependency 'mhacking'
+client_scripts {
+
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/tr.lua',
+	'locales/fr.lua',
+	'locales/tc.lua',
+	'config.lua',
+	'client/client.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/tr.lua',
+	'locales/fr.lua',
+	'locales/tc.lua',
+	'config.lua',
+	'server/server.lua'
+}
