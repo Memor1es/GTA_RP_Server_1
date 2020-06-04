6fa299f8d8f2b2a690305778d9b85c173ea8c9cf
YiShan8787 <59713684gx@gmail.com>
2020-06-04 16:12:34 +0800

    Create drug.sql

---
+++ server-data/resources/[esx]/[illegal]/stasiek_selldrugs/drug.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('goldbar', '金條', 50, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('silverbar', '銀條', 50, 0, 1);
+
+
+
+
