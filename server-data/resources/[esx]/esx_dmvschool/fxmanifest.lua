365e53d5936da4d04b0054ed6f7a15e083954c69
YiShan8787 <59713684gx@gmail.com>
2020-06-04 20:23:38 +0800

    dmv school

---
+++ server-data/resources/[esx]/esx_dmvschool/fxmanifest.lua
@@ -0,0 +1,51 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX DMV School'
+
+version '1.0.4'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/en.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'locales/tc.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/en.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'locales/tc.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+ui_page 'html/ui_tc.html'
+
+files {
+	'html/ui.html',
+	'html/logo.png',
+	'html/dmv.png',
+	'html/styles.css',
+	--'html/questions.js',
+	'html/questions_tc.js',
+	'html/scripts.js',
+	'html/debounce.min.js'
+}
+
+dependencies {
+	'es_extended',
+	'esx_license'
+}
---
+++ server-data/resources/[esx]/esx_dmvschool/html/questions_tc.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "如果您以 80 km/h 的速度行駛，並且正接近住宅區，則必須：",
+		propositionA: "加速",
+		propositionB: "如果不超車，則保持速度",
+		propositionC: "減速",
+		propositionD: "保持速度",
+		reponse: "C"
+	},
+
+	{
+		question: "如果您在紅綠燈處右轉，但看到行人經過時您該怎麼辦：",
+		propositionA: "加速輾過",
+		propositionB: "檢查附近是否沒有其他車輛",
+		propositionC: "等待行人通過馬路",
+		propositionD: "射擊行人並繼續行駛",
+		reponse: "C"
+	},
+
+	{
+		question: "若沒有任何速限標示，市區的速度限制為： __ km/h",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "在每次變換車道之前，您必須：",
+		propositionA: "確認後照鏡",
+		propositionB: "緩緩變換車道",
+		propositionC: "打方向燈",
+		propositionD: "以上皆是",
+		reponse: "D"
+	},
+
+	{
+		question: "多少血液酒精濃度會被視為酒駕？",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "0.06%",
+		reponse: "C"
+	},
+
+	{
+		question: "什麼時候可以在紅綠燈處繼續行駛？",
+		propositionA: "當它是綠燈時",
+		propositionB: "當路口沒人時",
+		propositionC: "當您在校區時",
+		propositionD: "當它是綠燈或是紅燈並且您向右轉時",
+		reponse: "D"
+	},
+
+	{
+		question: "有行人不遵守交通號誌時，不該怎麼做？",
+		propositionA: "讓他通過",
+		propositionB: "先觀察再繼續行駛",
+		propositionC: "揮手告知他們穿越",
+		propositionD: "繼續行駛，因為是綠燈",
+		reponse: "D"
+	},
+
+	{
+		question: "下列何者為您要超車時所被允許的？",
+		propositionA: "緊緊跟著對方以便更快的超車",
+		propositionB: "不離開原本車道的情況下超車",
+		propositionC: "從對向車道進行超車",
+		propositionD: "超過速限後進行超車",
+		reponse: "C"
+	},
+
+	{
+		question: "您正在高速公路上行駛，最高時限為 120 km/h。 但是其他大多數駕駛以每小時 125 公里的速度行駛，此時您的行駛速度不應超過:",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "110 km/h",
+		reponse: "A"
+	},
+
+	{
+		question: "當您被其他車輛超越時，請務必不要:",
+		propositionA: "減速",
+		propositionB: "檢查你的後照鏡",
+		propositionC: "看其他駕駛",
+		propositionD: "提高速度",
+		reponse: "D"
+	},
+]
