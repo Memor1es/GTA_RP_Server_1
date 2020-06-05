a2ffca7bece770f83a528ec23aa7e62829613542
jackchengrock <jackchengrock@gmail.com>
2020-06-06 01:14:54 +0800

    car

---
+++ server-data/resources/[esx]/[vehicle]/whiteplate/config.lua
@@ -0,0 +1,19 @@
+Config = {
+	czas = 15, -- Czas po którym widać tablice (minuty)
+	cooldown = 60, -- Czas jaki trzeba czekać aby jeszcze raz użyć komendy
+	money = 10000, -- Ile ma kosztować zdjęcia tablic	
+	coords = { x = 268.82, y = -1722.79, z = 29.18 }, -- Koordynaty warsztatu
+
+	-- Blip (https://docs.fivem.net/game-references/blips/)
+	blipEnabled = true, -- Czy ma się wyświetlać blip na mapie?
+	blipSprite = 402,
+	blipColour = 40,
+	blipScale = 0.8,
+	blipName = 'Warsztat Władka',
+	
+	-- Ped (https://wiki.rage.mp/index.php?title=Peds)
+	pedName = 'a_m_m_hillbilly_01',
+	pedHash = 0x6C9B2849,
+	pedHeading = 7.0,
+	pedCoords = { x = 270.36, y = -1727.04, z = 29.26 },
+}
