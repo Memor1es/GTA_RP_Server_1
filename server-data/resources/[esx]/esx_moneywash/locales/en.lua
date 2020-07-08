90351d8ab3812d58eb3d2d111a4b8ecf91fcfc48
jackchengrock <jackchengrock@gmail.com>
2020-07-09 02:18:29 +0800

    moneywash

---
+++ server-data/resources/[esx]/esx_moneywash/locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+	['you_have_washed'] 		= '洗乾淨了~r~$',
+	['you_have_washed_waiting'] = '身上有 ~r~髒衣服. ~s~時間:~b~  ',
+	['dirty_money'] 			= '髒衣服',
+	['you_have_received'] 		= ' ~s~已經洗了 ~g~',
+	['clean_money'] 			= '乾淨衣服',
+	['invalid_amount']			= '輸入錯誤數量',
+	['press_menu'] 				= '按下~INPUT_CONTEXT~ 來 ~r~洗衣服~s~',
+	['no'] 						= '否',
+	['wash_money']				= '洗衣服',
+	['washed_menu']				= '衣服清洗中',
+	['wash_money_amount']		= '衣服數量'
+}
