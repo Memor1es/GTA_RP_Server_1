d28a7df0a2b068a10076e3cb87df6ea3337d3821
jackchengrock <jackchengrock@gmail.com>
2020-07-10 07:36:07 +0800

    phone

---
+++ server-data/resources/[phone]/gcphone/config.lua
@@ -0,0 +1,9 @@
+FixePhone = {
+  -- -- Poste de police
+  ['911'] = { name =  "報案三聯單", coords = { x = 441.2, y = -979.7, z = 30.58 } },
+  
+  -- -- Cabine proche du poste de police
+  ['000'] = { name = "掛號櫃台", coords = { x = 306.47, y = -597.46, z = 43.29 } },
+}
+
+ShowNumberNotification = true -- Show Number or Contact Name when you receive new SMS
