2ce8ab5d6f5ae6c35235d9fda58486573ab989dc
jackchengrock <jackchengrock@gmail.com>
2020-07-10 06:22:34 +0800

    radio

--- server-data/resources/[esx]/[voice]/ls-radio/config.lua
+++ server-data/resources/[esx]/[voice]/ls-radio/config.lua
@@ -5,11 +5,11 @@ Config.enableCmd = true --  /radio command should be active or not (if not you h
 
 Config.messages = {
 
-  ['not_on_radio'] = 'You are currently not on any radio',
-  ['on_radio'] = 'You are currently on the radio: <b>',
-  ['joined_to_radio'] = 'You joined the radio: <b>',
-  ['restricted_channel_error'] = 'You can not join encrypted channels!',
-  ['you_on_radio'] = 'You are already on the radio: <b>',
-  ['you_leave'] = 'You left the radio: <b>'
+  ['not_on_radio'] = '不在無線電頻道裡',
+  ['on_radio'] = '進入無線電頻道: <b>',
+  ['joined_to_radio'] = '進入頻道: <b>',
+  ['restricted_channel_error'] = '無法進入加密頻道!',
+  ['you_on_radio'] = '已經加入頻道: <b>',
+  ['you_leave'] = '離開頻道: <b>'
 
 }
