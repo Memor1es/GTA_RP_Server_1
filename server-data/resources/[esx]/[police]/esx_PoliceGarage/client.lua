e0fe6a0597fa6f0624cd30b611c0e2ba9524ce04
jackchengrock <jackchengrock@gmail.com>
2020-07-10 01:22:02 +0800

    garage

--- server-data/resources/[esx]/[police]/esx_PoliceGarage/client.lua
+++ server-data/resources/[esx]/[police]/esx_PoliceGarage/client.lua
@@ -522,3 +522,9 @@ function OpenColorMenu(type, value, colortype)
 		menu.close()
 	end)
 end
+
+
+RegisterCommand('livery', function(source, args) 
+	local Veh = GetVehiclePedIsIn(GetPlayerPed(-1)) 
+	local liveryID = tonumber(args[1]) SetVehicleLivery(Veh, liveryID) 
+end, false)
