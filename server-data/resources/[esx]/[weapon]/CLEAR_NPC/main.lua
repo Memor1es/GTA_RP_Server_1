1d96160d8bd711b319417d51f622754285e0a901
jackchengrock <jackchengrock@gmail.com>
2020-07-11 04:32:14 +0800

    Update main.lua

--- server-data/resources/[esx]/[weapon]/CLEAR_NPC/main.lua
+++ server-data/resources/[esx]/[weapon]/CLEAR_NPC/main.lua
@@ -20,11 +20,11 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0) -- prevent crashing
 
 		-- These natives have to be called every frame.
-		SetVehicleDensityMultiplierThisFrame(0.0) -- set traffic density to 0 
-		SetPedDensityMultiplierThisFrame(0.0) -- set npc/ai peds density to 0
-		SetRandomVehicleDensityMultiplierThisFrame(0.0) -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
-		SetParkedVehicleDensityMultiplierThisFrame(0.0) -- set random parked vehicles (parked car scenarios) to 0
-		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0) -- set random npc/ai peds or scenario peds to 0
+		SetVehicleDensityMultiplierThisFrame(4.0) -- set traffic density to 0 
+		SetPedDensityMultiplierThisFrame(4.0) -- set npc/ai peds density to 0
+		SetRandomVehicleDensityMultiplierThisFrame(2.0) -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
+		SetParkedVehicleDensityMultiplierThisFrame(2.0) -- set random parked vehicles (parked car scenarios) to 0
+		SetScenarioPedDensityMultiplierThisFrame(4.0, 4.0) -- set random npc/ai peds or scenario peds to 0
 		SetGarbageTrucks(false) -- Stop garbage trucks from randomly spawning
 		SetRandomBoats(false) -- Stop random boats from spawning in the water.
 		SetCreateRandomCops(false) -- disable random cops walking/driving around.
