810d13971c650e1762d7eba50db84c93fb0ae4ee
jackchengrock <jackchengrock@gmail.com>
2020-07-11 05:04:42 +0800

    Update main.lua

--- server-data/resources/[esx]/[weapon]/CLEAR_NPC/main.lua
+++ server-data/resources/[esx]/[weapon]/CLEAR_NPC/main.lua
@@ -15,6 +15,38 @@ Citizen.CreateThread(function()
  
 end)
 
+local dispatchTypes =
+{
+	DT_PoliceAutomobile = { ID = 1, ACTIVE = false },
+	DT_PoliceHelicopter = { ID = 2, ACTIVE = false },
+	DT_FireDepartment = { ID = 3, ACTIVE = false },
+	DT_SwatAutomobile = { ID = 4, ACTIVE = false },
+	DT_AmbulanceDepartment = { ID = 5, ACTIVE = false },
+	DT_PoliceRiders = { ID = 6, ACTIVE = false },
+	DT_PoliceVehicleRequest = { ID = 7, ACTIVE = false },
+	DT_PoliceRoadBlock = { ID = 8, ACTIVE = false},
+	DT_PoliceAutomobileWaitPulledOver = { ID = 9, ACTIVE = false },
+	DT_PoliceAutomobileWaitCruising = { ID = 10, ACTIVE = false },
+	DT_Gangs = { ID = 11, ACTIVE = false },
+	DT_SwatHelicopter = { ID = 12, ACTIVE = false },
+	DT_PoliceBoat = { ID = 13, ACTIVE = false },
+	DT_ArmyVehicle = { ID = 14, ACTIVE = false },
+	DT_BikerBackup = { ID = 15, ACTIVE = false }
+}
+
+Citizen.CreateThread(function()
+	while true do
+		Wait(0)
+		if NetworkIsPlayerActive(PlayerId()) then
+			for k,v in pairs(dispatchTypes) do
+				EnableDispatchService(dispatchTypes[k].ID, dispatchTypes[k].ACTIVE)
+			end
+			SetMaxWantedLevel(0)
+			break;
+		end
+	end
+end)
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0) -- prevent crashing
@@ -29,7 +61,8 @@ Citizen.CreateThread(function()
 		SetRandomBoats(false) -- Stop random boats from spawning in the water.
 		SetCreateRandomCops(false) -- disable random cops walking/driving around.
 		SetCreateRandomCopsNotOnScenarios(false) -- stop random cops (not in a scenario) from spawning.
-		SetCreateRandomCopsOnScenarios(false) -- stop random cops (in a scenario) from spawning.
+        SetCreateRandomCopsOnScenarios(false) -- stop random cops (in a scenario) from spawning.
+        SetScenarioTypeEnabled(scenario_name, false)
 		
 		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
 		ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
