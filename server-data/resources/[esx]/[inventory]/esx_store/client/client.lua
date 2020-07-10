28734a8e3024d41a28fb7da3f3c5ed9313672ee9
YiShan8787 <59713684gx@gmail.com>
2020-07-10 21:58:08 +0800

    store

---
+++ server-data/resources/[esx]/[inventory]/esx_store/client/client.lua
@@ -0,0 +1,161 @@
+ESX = nil
+local ShowText = false
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+    end
+end)
+
+--put in 
+function OpenPlayerInventoryMenu(property, owner)
+	ESX.TriggerServerCallback('esx_property:getPlayerInventory', function(inventory)
+		local elements = {}
+
+		if inventory.blackMoney > 0 then
+			table.insert(elements, {
+				label = _U('dirty_money', ESX.Math.GroupDigits(inventory.blackMoney)),
+				type  = 'item_account',
+				value = 'black_money'
+			})
+		end
+
+		for i=1, #inventory.items, 1 do
+			local item = inventory.items[i]
+
+			if item.count > 0 then
+				table.insert(elements, {
+					label = item.label .. ' x' .. item.count,
+					type  = 'item_standard',
+					value = item.name
+				})
+			end
+		end
+
+		for i=1, #inventory.weapons, 1 do
+			local weapon = inventory.weapons[i]
+
+			table.insert(elements, {
+				label = weapon.label .. ' [' .. weapon.ammo .. ']',
+				type  = 'item_weapon',
+				value = weapon.name,
+				ammo  = weapon.ammo
+			})
+		end
+
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'player_inventory', {
+			title    = property.label .. ' - ' .. _U('inventory'),
+			align    = 'top-left',
+			elements = elements
+		}, function(data, menu)
+
+			if data.current.type == 'item_weapon' then
+				menu.close()
+				TriggerServerEvent('esx_store:putItem', owner, data.current.type, data.current.value, data.current.ammo)
+
+				ESX.SetTimeout(300, function()
+					OpenPlayerInventoryMenu(property, owner)
+				end)
+			else
+				ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'put_item_count', {
+					title = _U('amount')
+				}, function(data2, menu2)
+					local quantity = tonumber(data2.value)
+
+					if quantity == nil then
+						ESX.ShowNotification(_U('amount_invalid'))
+					else
+						menu2.close()
+
+						TriggerServerEvent('esx_store:putItem', owner, data.current.type, data.current.value, tonumber(data2.value))
+						ESX.SetTimeout(300, function()
+							OpenPlayerInventoryMenu(property, owner)
+						end)
+					end
+				end, function(data2, menu2)
+					menu2.close()
+				end)
+			end
+		end, function(data, menu)
+			menu.close()
+		end)
+	end)
+end
+
+--take out
+
+function OpenRoomInventoryMenu(property, owner)
+	ESX.TriggerServerCallback('esx_property:getPropertyInventory', function(inventory)
+		local elements = {}
+
+		if inventory.blackMoney > 0 then
+			table.insert(elements, {
+				label = _U('dirty_money', ESX.Math.GroupDigits(inventory.blackMoney)),
+				type = 'item_account',
+				value = 'black_money'
+			})
+		end
+
+		for i=1, #inventory.items, 1 do
+			local item = inventory.items[i]
+
+			if item.count > 0 then
+				table.insert(elements, {
+					label = item.label .. ' x' .. item.count,
+					type = 'item_standard',
+					value = item.name
+				})
+			end
+		end
+
+		for i=1, #inventory.weapons, 1 do
+			local weapon = inventory.weapons[i]
+
+			table.insert(elements, {
+				label = ESX.GetWeaponLabel(weapon.name) .. ' [' .. weapon.ammo .. ']',
+				type  = 'item_weapon',
+				value = weapon.name,
+				index = i
+			})
+		end
+
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'room_inventory', {
+			title    = property.label .. ' - ' .. _U('inventory'),
+			align    = 'top-left',
+			elements = elements
+		}, function(data, menu)
+
+			if data.current.type == 'item_weapon' then
+				menu.close()
+
+				TriggerServerEvent('esx_store:getItem', owner, data.current.type, data.current.value, data.current.index)
+				ESX.SetTimeout(300, function()
+					OpenRoomInventoryMenu(property, owner)
+				end)
+			else
+				ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'get_item_count', {
+					title = _U('amount')
+				}, function(data2, menu)
+
+					local quantity = tonumber(data2.value)
+					if quantity == nil then
+						ESX.ShowNotification(_U('amount_invalid'))
+					else
+						menu.close()
+
+						TriggerServerEvent('esx_store:getItem', owner, data.current.type, data.current.value, quantity)
+						ESX.SetTimeout(300, function()
+							OpenRoomInventoryMenu(property, owner)
+						end)
+					end
+				end, function(data2,menu)
+					menu.close()
+				end)
+			end
+		end, function(data, menu)
+			menu.close()
+		end)
+	end, owner)
+end
+
---
+++ server-data/resources/[esx]/[inventory]/esx_store/server/server.lua
@@ -0,0 +1,121 @@
+local ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterUsableItem('watch', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.triggerEvent('esx_watch:ShowTime')
+end)
+
+
+RegisterNetEvent('esx_property:putItem')
+AddEventHandler('esx_property:putItem', function(owner, type, item, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local xPlayerOwner = ESX.GetPlayerFromIdentifier(owner)
+
+	if type == 'item_standard' then
+		local playerItemCount = xPlayer.getInventoryItem(item).count
+
+		if playerItemCount >= count and count > 0 then
+			TriggerEvent('esx_addoninventory:getInventory', 'property', xPlayerOwner.identifier, function(inventory)
+				xPlayer.removeInventoryItem(item, count)
+				inventory.addItem(item, count)
+				xPlayer.showNotification(_U('have_deposited', count, inventory.getItem(item).label))
+			end)
+		else
+			xPlayer.showNotification(_U('invalid_quantity'))
+		end
+	elseif type == 'item_account' then
+		if xPlayer.getAccount(item).money >= count and count > 0 then
+			xPlayer.removeAccountMoney(item, count)
+
+			TriggerEvent('esx_addonaccount:getAccount', 'property_' .. item, xPlayerOwner.identifier, function(account)
+				account.addMoney(count)
+			end)
+		else
+			xPlayer.showNotification(_U('amount_invalid'))
+		end
+	elseif type == 'item_weapon' then
+		if xPlayer.hasWeapon(item) then
+			xPlayer.removeWeapon(item)
+
+			TriggerEvent('esx_datastore:getDataStore', 'property', xPlayerOwner.identifier, function(store)
+				local storeWeapons = store.get('weapons') or {}
+
+				table.insert(storeWeapons, {
+					name = item,
+					ammo = count
+				})
+
+				store.set('weapons', storeWeapons)
+			end)
+		end
+	end
+end)
+
+
+RegisterNetEvent('esx_store:getItem')
+AddEventHandler('esx_store:getItem', function(owner, type, item, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local xPlayerOwner = ESX.GetPlayerFromIdentifier(owner)
+
+	if type == 'item_standard' then
+		TriggerEvent('esx_addoninventory:getInventory', 'property', xPlayerOwner.identifier, function(inventory)
+			local inventoryItem = inventory.getItem(item)
+
+			-- is there enough in the property?
+			if count > 0 and inventoryItem.count >= count then
+				-- can the player carry the said amount of x item?
+				if xPlayer.canCarryItem(item, count) then
+					inventory.removeItem(item, count)
+					xPlayer.addInventoryItem(item, count)
+					xPlayer.showNotification(_U('have_withdrawn', count, inventoryItem.label))
+				else
+					xPlayer.showNotification(_U('player_cannot_hold'))
+				end
+			else
+				xPlayer.showNotification(_U('not_enough_in_property'))
+			end
+		end)
+	elseif type == 'item_account' then
+		TriggerEvent('esx_addonaccount:getAccount', 'property_' .. item, xPlayerOwner.identifier, function(account)
+			if account.money >= count then
+				account.removeMoney(count)
+				xPlayer.addAccountMoney(item, count)
+			else
+				xPlayer.showNotification(_U('amount_invalid'))
+			end
+		end)
+	elseif type == 'item_weapon' then
+		--[[TriggerEvent('esx_datastore:getDataStore', 'property', xPlayerOwner.identifier, function(store)
+			local storeWeapons = store.get('weapons') or {}
+			local weapon = storeWeapons[data.current.count] -- count is the index
+
+			if weapon then
+				if not xPlayer.hasWeapon(weaponName) then
+					table.remove(storeWeapons, data.current.count)
+					store.set('weapons', storeWeapons)
+
+					xPlayer.addWeapon(weapon.name, weapon.ammo)
+				end
+			end
+		end)--]]
+		TriggerEvent('esx_datastore:getDataStore', 'property', xPlayerOwner.identifier, function(store)
+			local storeWeapons = store.get('weapons') or {}
+			local weaponName   = nil
+			local ammo         = nil
+	
+			for i=1, #storeWeapons, 1 do
+				if storeWeapons[i].name == item then
+					weaponName = storeWeapons[i].name
+					ammo       = storeWeapons[i].ammo
+	
+					table.remove(storeWeapons, i)
+					break
+				end
+			end
+	
+			store.set('weapons', storeWeapons)
+			xPlayer.addWeapon(weaponName, ammo)
+		end)
+	end
+end)
