d64c5c7c01780010ce380a32fcb8ec8f97a63763
YiShan8787 <59713684gx@gmail.com>
2020-07-10 21:12:26 +0800

    locale

--- server-data/resources/[esx]/[ui]/esx_changeclothe/config.lua
+++ server-data/resources/[esx]/[ui]/esx_changeclothe/config.lua
@@ -5,6 +5,8 @@ Config.MarkerType                 = 1
 Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 0.5 }
 Config.MarkerColor                = { r = 50, g = 50, b = 204 }
 
+Config.Locale                     = 'tc'
+
 Config.EnableCustomPeds           = false
 
 Config.place                      = {
