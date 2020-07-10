9b28941a6e5a8a21784f69e47b5ed8cb75557c24
jackchengrock <jackchengrock@gmail.com>
2020-07-10 22:52:00 +0800

    Update config.lua

--- server-data/resources/[phone]/gcphone/config.lua
+++ server-data/resources/[phone]/gcphone/config.lua
@@ -4,6 +4,10 @@ FixePhone = {
   
   -- -- Cabine proche du poste de police
   ['000'] = { name = "掛號櫃台", coords = { x = 306.47, y = -597.46, z = 43.29 } },
+
+  ['111'] = { name = "公共電話", coords = { x = 1782.95, y = 2572.87, z = 45.8 } },
+  ['222'] = { name = "公共電話", coords = { x = 1781.36, y = 2572.77, z = 45.8 } },
+  ['333'] = { name = "公共電話", coords = { x = 1776.1, y = 2572.87, z = 45.8 } },
 }
 
 ShowNumberNotification = true -- Show Number or Contact Name when you receive new SMS
