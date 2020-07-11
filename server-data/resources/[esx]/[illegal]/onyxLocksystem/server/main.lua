af5567055266af0be86e9d24bb7232eb17fa9ca5
HoKuanYu <40305418+HoKuanYu@users.noreply.github.com>
2020-07-12 02:30:23 +0800

    Update main.lua

--- server-data/resources/[esx]/[illegal]/onyxLocksystem/server/main.lua
+++ server-data/resources/[esx]/[illegal]/onyxLocksystem/server/main.lua
@@ -20,11 +20,11 @@ AddEventHandler('onyx:reqHotwiring', function(plate)
         local rnd = math.random(1, 2)
         if rnd == 1 then
             xPlayer.removeInventoryItem('lockpick', 1)
-            xPlayer.showNotification("你的 ~o~開鎖工具 ~r~壞掉 ~s~了")
+            xPlayer.showNotification("你的 ~y~開鎖工具 ~r~壞掉 ~s~了")
             -- TriggerClientEvent('mythic_notify:client:SendAlert', source, {type = 'error', text = 'Your lockpick has broken'})
         end
     else
-        xPlayer.showNotification("你身上 ~r~沒有 ~o~開鎖工具")
+        xPlayer.showNotification("你身上 ~r~沒有 ~y~開鎖工具")
         -- TriggerClientEvent('mythic_notify:client:SendAlert', source, {type = 'error', text = 'You have no lockpicks'})
     end
 end)
