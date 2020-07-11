de47fb81151b47b1d38b0972749252bb7bda4116
Yi-Hsuan-Su <40382478+Yi-Hsuan-Su@users.noreply.github.com>
2020-07-11 21:17:33 +0800

    server

--- server-data/resources/[easy]/ooc/server.lua
+++ server-data/resources/[easy]/ooc/server.lua
@@ -22,10 +22,41 @@ function stringsplit(inputstr, sep)
     return t
 end
 
-ESX.RegisterCommand('ooc', 'user', function(xPlayer, args, showError)
-    --args.playerId.triggerEvent('esx_ambulancejob:revive')	
-    TriggerClientEvent('chatMessage', -1, "OOC | " .. xPlayer.name, { 128, 128, 128 }, args.message)
+function getIdentity(source)
+	local identifier = string.sub(GetPlayerIdentifiers(source)[2], 9)
+    local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {['@identifier'] = identifier})
+	if result[1] ~= nil then
+		local identity = result[1]
 
+		return {
+			identifier = identity['identifier'],
+			firstname = identity['firstname'],
+			lastname = identity['lastname'],
+			dateofbirth = identity['dateofbirth'],
+			sex = identity['sex'],
+			height = identity['height']
+			
+		}
+	else
+        return nil
+    end
+end
+
+ESX.RegisterCommand('ooc', 'user', function(xPlayer, args, showError)
+    --args.playerId.triggerEvent('esx_ambulancejob:revive')
+    local source = xPlayer.source
+    local name = getIdentity(source)
+    
+    if string.match(name.firstname, "[A-Za-z]") ~= nil then
+        TriggerClientEvent('chat:addMessage', -1, {
+            args = { '^2' .. name['firstname'] .. ' ' .. name['lastname'] .. ' [' .. source .. ']', '^0 ' .. args.message }
+        })
+    else
+        TriggerClientEvent('chat:addMessage', -1, {
+            args = { '^2' .. name['lastname'] .. name['firstname'] .. ' [' .. source .. ']', '^0 ' .. args.message }
+        })
+    end
+    -- TriggerClientEvent('chatMessage', -1, "OOC | " .. xPlayer.name, { 128, 128, 128 }, args.message)
 end, true, {help = "RP外發言(out of character)", validate = true, arguments = {
     {name = 'message', help = "想要輸入的訊息", type = 'string'}
 
